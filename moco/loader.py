4f30b2a3318c79c70a6ec4e1290241fcd4aad231
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2020-12-14 18:46:18 +0800

    ThreeCrop

--- moco/loader.py
+++ moco/loader.py
@@ -1,6 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from PIL import ImageFilter
 import random
+import torchvision.transforms as transforms
 
 
 class TwoCropsTransform:
@@ -13,6 +14,41 @@ class TwoCropsTransform:
         q = self.base_transform(x)
         k = self.base_transform(x)
         return [q, k]
+
+
+class CenterCropLongEdge(object):
+  """Crops the given PIL Image on the long edge.
+  Args:
+      size (sequence or int): Desired output size of the crop. If size is an
+          int instead of sequence like (h, w), a square crop (size, size) is
+          made.
+  """
+  def __call__(self, img):
+    """
+    Args:
+        img (PIL Image): Image to be cropped.
+    Returns:
+        PIL Image: Cropped image.
+    """
+    return transforms.functional.center_crop(img, min(img.size))
+
+  def __repr__(self):
+    return self.__class__.__name__
+
+
+class ThreeCropsTransform:
+    """Take two random crops of one image as the query and key."""
+
+    def __init__(self, base_transform, gan_transform):
+        self.base_transform = base_transform
+        self.gan_transform = gan_transform
+
+    def __call__(self, x):
+        q = self.base_transform(x)
+        k = self.base_transform(x)
+        g = self.gan_transform(x)
+        return [q, k, g]
+
 
 
 class GaussianBlur(object):
